4b0a5c0dcca3435eaaa46c6ece67efbb93c5d3bd
Renuupendra <renuupendrasulthan1@gmail.com>
2026-03-30 20:05:41 -0400

    initial commit

---
+++ src/test/java/com/humber/shop/ShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humber.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
